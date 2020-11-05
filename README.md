008c977a71c79c24d5458abdfb88e9633e7309af
Perianu Leon <perianu.leon@outlook.com>
2020-11-05 23:47:07 +0200

    Final version of the program. Ambiental value might be wrong, but it is night.

--- README.md
+++ README.md
@@ -3,3 +3,13 @@
 This is a project for my custom desk I work on.
 I gathered the codes of my remote control.
 Feel free to use my code to test it for your implementation.
+
+Logic of the program
+PC off LIGHT off AMBIENTAL off   -> do nothing  		
+PC off LIGHT off AMBIENTAL on    -> do nothing   					
+PC off LIGHT on  AMBIENTAL off   -> setLight off		
+PC off LIGHT on  AMBIENTAL on    -> setLight off			
+PC on  LIGHT off AMBIENTAL off   -> setLight on			
+PC on  LIGHT off AMBIENTAL on    -> do nothing							
+PC on  LIGHT on  AMBIENTAL off   -> do nothing						
+PC on  LIGHT on  AMBIENTAL on    -> setLight off			
--- main.ino
+++ main.ino
@@ -1,52 +1,69 @@
 #include <IRremote.h>
 
+#define DEBUG false
+
 IRsend irsend;
 
-int64_t PC_LIGHT = 0;
-int64_t LIGHT = 0;
-int64_t AMBIENTAL = 0;
-
-#define debug false
-#define ndebug true
+unsigned int PC_LIGHT = 0;
+unsigned int LIGHT = 0;
+unsigned int AMBIENTAL = 0;
 
 bool checkPcLight() {
-	return true;
+	if(PC_LIGHT > 100) {
+		return true;
+	}
+	return false;
 }
 
 bool checkAmbientalLight() {
-	return true;
+	if(AMBIENTAL > 22) {
+		return true;
+	}
+	return false;
 }
 
 bool checkLights() {
-	return true;
+	if(LIGHT > 40) {
+		return true;
+	}
+	return false;
 }
 
-
-
 void getData() {
+	// int64_t sensorVal = analogRead(0);
+	
+	float LIGHT_VOUT = float(analogRead(0)) * (5/float(1023));
+	LIGHT = 500/(((9620 * (5-LIGHT_VOUT))/LIGHT_VOUT)/1000);
 
+	float PC_LIGHT_VOUT = float(analogRead(1)) * (5/float(1023));
+	PC_LIGHT = 500/(((10400 * (5-PC_LIGHT_VOUT))/PC_LIGHT_VOUT)/1000);
+
+	float AMBIENTAL_VOUT = float(analogRead(2)) * (5/float(1023));
+	AMBIENTAL = 500/(((10000 * (5-AMBIENTAL_VOUT))/AMBIENTAL_VOUT)/1000);
 }
 
 void setLightOff() {
 	while(checkLights()) {
 		irsend.sendNEC(0x00FF02FD, 32);
-		getData()
-		delay(50);
+		delay(100);
+		getData();
+		
 	}
 }
 
 void setLightOn() {
 	while(!checkLights()) {
 		irsend.sendNEC(0x00FF02FD, 32);
-		getData()
-		delay(50);
+		delay(100);
+		getData();
+		
 	}
 }
 
 void setup() { 
 	Serial.begin(9600); 
 	Serial.println("Program Started!");
-	getData(){}
+	getData();
 } 
 
 void goToSleep() {
@@ -54,7 +71,7 @@ void goToSleep() {
 		irsend.sendNEC(0x00FF1AE5, 32);
 		delay(50);
 	}
-	for(int i=0; i<5) {
+	for(int i=0; i<5; i++) {
 		irsend.sendNEC(0x00FFBA45, 32);
 		delay(1000);
 	}
@@ -85,15 +102,8 @@ void setBlue() {
 
 void loop() { 
 
-	// pc off light off ambiental off   					0
-	// pc off light off ambiental on	   					1
-	// pc off light on ambiental off -> setLight off		1
-	// pc off light on ambiental on -> setLight off			2
-	// pc on light off ambintal off -> setLight on			1
-	// pc on light off ambiental on							2
-	// pc on light on ambiental off							2
-	// pc on light on ambiental on -> setLight off			3
-	#if debug
+	getData();
+	#if !DEBUG
 		if(!checkPcLight() & checkLights() & !checkAmbientalLight()) {
 			goToSleep();
 			setLightOff();
@@ -104,14 +114,34 @@ void loop() {
 		}
 		else if (checkPcLight() & !checkLights() & !checkAmbientalLight()) {
 			setLightOn();
+			delay(50);
 			setDefaultProfile();
 		}
 		else if (checkPcLight() & checkLights() & checkAmbientalLight()) {
 			setBlue();
 			setLightOff();
 		}
-		delay(60000);
-		
+		delay(10000);
+	
+	#elif DEBUG
+	
+	Serial.print("PC Light: ");
+	Serial.print(PC_LIGHT);
+	Serial.print("   ");
+	Serial.println(checkPcLight());
+	
+	Serial.print("Ambiental light: ");
+	Serial.print(AMBIENTAL);
+	Serial.print("   ");
+	Serial.println(checkAmbientalLight());
+
+	Serial.print("Light: ");
+	Serial.print(LIGHT);
+	Serial.print("   ");
+	Serial.println(checkLights());
+
+	delay(2000);
+
 	#endif
-	getData()
+
 }
