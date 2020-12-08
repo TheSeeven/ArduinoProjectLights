58aee5a73787cd505af4f46610be8c7270a337c7
Perianu Leon <perianu.leon@outlook.com>
2020-12-09 01:34:02 +0200

    The sensors data id calculated in an average manner. Minor bug fixes (Adjusted the threshold)

--- main.ino
+++ main.ino
@@ -4,9 +4,65 @@
 
 IRsend irsend;
 
+struct DataPack {
+	unsigned int PC_LIGHT = 0;
+	unsigned int LIGHT = 0;
+	unsigned int AMBIENTAL = 0;
+
+	DataPack(unsigned int PC_LIGHT, unsigned int LIGHT, unsigned int AMBIENTAL) {
+		this->PC_LIGHT = PC_LIGHT;
+		this->LIGHT = LIGHT;
+		this->AMBIENTAL = AMBIENTAL;
+	}
+
+	DataPack() {
+		this->PC_LIGHT = 0;
+		this->LIGHT = 0;
+		this->AMBIENTAL = 0;
+	}
+
+	~DataPack() {}
+
+	void normalize() {
+		this.AMBIENTAL = this.AMBIENTAL / 4;
+		this.LIGHT = this.LIGHT / 4;
+		this.PC_LIGHT = this.PC_LIGHT / 4;
+	}
+
+	void write() {
+		PC_LIGHT = this.PC_LIGHT;
+		LIGHT = this.LIGHT;
+		AMBIENTAL = this.AMBIENTAL;
+	}
+}
+
+
 unsigned int PC_LIGHT = 0;
 unsigned int LIGHT = 0;
 unsigned int AMBIENTAL = 0;
+
+DataPack DATA_SAMPLES[] = new DataPack[4];
+int DATA_SAMPLES_COUNTER = 0;
+
+void addSample(DataPack data) {
+	DATA_SAMPLES[DATA_SAMPLES_COUNTER] = data;
+	DATA_SAMPLES_COUNTER++;
+}
+
+void setAverageData() {
+	DataPack result = new DataPack();
+	for (int i = 0; i < sizeof(DATA_SAMPLES) / sizeof(DataPack); i++) {
+		result->AMBIENTAL += DATA_SAMPLES[i]->AMBIENTAL;
+		result->LIGHT += DATA_SAMPLES[i]->LIGHT;
+		result->PC_LIGHT += DATA_SAMPLES[i]->PC_LIGHT;
+		DATA_SAMPLES[i].~DataPack();
+	}
+	result.normalize();
+	result.write();
+	DATA_SAMPLES_COUNTER = 0;
+	result.~DataPack();
+}
+
 
 bool checkPcLight() {
 	if(PC_LIGHT > 100) {
@@ -30,14 +86,20 @@ bool checkLights() {
 }
 
 void getData() {
-	float LIGHT_VOUT = float(analogRead(0)) * (5/float(1023));
-	LIGHT = 500/(((9620 * (5-LIGHT_VOUT))/LIGHT_VOUT)/1000);
+	for (i = 0; i < 4;i++)
+	{
+		float LIGHT_VOUT = float(analogRead(0)) * (5 / float(1023));
+		unsigned int temp_LIGHT = 500 / (((9620 * (5 - LIGHT_VOUT)) / LIGHT_VOUT) / 1000);
 
-	float PC_LIGHT_VOUT = float(analogRead(1)) * (5/float(1023));
-	PC_LIGHT = 500/(((10400 * (5-PC_LIGHT_VOUT))/PC_LIGHT_VOUT)/1000);
+		float PC_LIGHT_VOUT = float(analogRead(1)) * (5/float(1023));
+		unsigned int temp_PC_LIGHT = 500 / (((10400 * (5 - PC_LIGHT_VOUT)) / PC_LIGHT_VOUT) / 1000);
 
-	float AMBIENTAL_VOUT = float(analogRead(2)) * (5/float(1023));
-	AMBIENTAL = 500/(((10000 * (5-AMBIENTAL_VOUT))/AMBIENTAL_VOUT)/1000);
+		float AMBIENTAL_VOUT = float(analogRead(2)) * (5/float(1023));
+		unsigned int temp_AMBIENTAL = 500 / (((10000 * (5 - AMBIENTAL_VOUT)) / AMBIENTAL_VOUT) / 1000);
+		addSample(new DataPack(temp_PC_LIGHT, temp_LIGHT, temp_AMBIENTAL))
+		delay(275);
+	}
+	setAverageData();
 }
 
 void setLightOff() {
@@ -129,7 +191,7 @@ void loop() {
 			setBlue();
 			setLightOff();
 		}
-		delay(10000);
+		delay(7000);
 	
 	#elif DEBUG
 	
